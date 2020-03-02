75a5752c0aa1499976b755247760681604b50539
Anvar <ankhafizov@yandex.ru>
2020-03-02 23:50:24 +0300

    Revert "enables to view phantoms grom shelve"
    This reverts commit 5ae4f1c0d6ae959d72be5ea800462888b5284e45.

--- viewer.py
+++
@@ -1,6 +0,0 @@
-import matplotlib.pyplot as plt
-img = opener.open('dim2,porsty0.3,blobns2,noise0.05,angles180_test')['processed']
-
-_, ax = plt.subplots(figsize = (7,7))
-ax.imshow(img, cmap = 'gray')
-plt.show()
