af78791769b9e403234ee088d7921dbe1a825022
Anvar <ankhafizov@yandex.ru>
2020-03-07 19:33:17 +0300

    now saves files using data_manager.py

--- pixel_counter.py
+++ pixel_counter.py
@@ -1,31 +1,69 @@
-import numpy as np
-import opener
-import pandas as pd
-import os
+import data_manager as dm
 from scipy import ndimage
+import numpy as np
+import pandas as pd
 
 
-def count_neighbor_average_array(arr):
+def count_neighbor_average_array(arr: np.ndarray):
+    '''
+    For each single pixel counts average intensity value of it surroundings (adjacent pixels).
+
+    Parameters:
+    -----------
+    arr: ndarray.
+        Array with pixel intensities (e.g. PoreSpy phantom images)
+    
+    Results:
+    -----------
+    out: ndarray
+        array with the average values
+    '''
+
     dim = len(arr.shape)
     number, kern = (26, [[[0]]]) if dim == 3 else (8, [[0]])
-    
+
     kern = np.pad(kern, (1,1), constant_values=(1, 1))
-    filt_arr = ndimage.convolve(arr, kern)/number
+    av_arr = ndimage.convolve(arr, kern)/number
     
-    return filt_arr
+    return av_arr
 
 
-def save_data(phantom_file_key, tag):
-    phantoms = opener.open(phantom_file_key)
-    orig_phantom = phantoms['original']
-    proc_phantom = phantoms['processed']
+def count_neighbor_average_array_and_save(dimension:int,
+                                        id_indx: int, 
+                                        tag:str):
+    '''
+    Use this function to count neighbour pixels' average intensities and save the csv 
+    file for ML process.
+    Use show_data_info function to find out dimension, id_inx and tags, which exist.
+
+    Parameters:
+    -----------
+    dimension: 2 or 3. 
+        Dimension of phantom Euclidian space
+
+    id_indx: 1,2,3,etc.
+        id for phantom with certain porosity, blobiness and experiment parameters
+
+    tag: 'test', 'train' or another
+        This parameter controls conflicts if several csv files are generated for 1 phanom.
+        Keep it different for staging different images with similar parameters
+    
+    results:
+    --------
+    out: csv file
+        See in the  script's directory in the folder 'database'
+        To open use data_manager.get_data(args) function
+    '''
+
+    orig_phantom = dm.get_data(dimension, id_indx, tag, 'orig_phantom')
+    proc_phantom = dm.get_data(dimension, id_indx, tag, 'processed_phantom')
 
     neighbor_pixel_average = count_neighbor_average_array(proc_phantom)
+
     data = {'neighbor_average': np.concatenate(neighbor_pixel_average),
             'proc_phantom_pixel_values': np.concatenate(proc_phantom),
             'pixel_real_value': np.concatenate(orig_phantom)}
-    df = pd.DataFrame(data)
+    csv_file = pd.DataFrame(data)
 
-    save_path = os.path.dirname(os.path.realpath(__file__))+f'\\__pycache__\\{tag}_pixel_dataframe.csv'
-    df.to_csv(save_path)
+    dm.add_csv(dimension, id_indx, tag, csv_file)
 
