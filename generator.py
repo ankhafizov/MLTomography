d5d271d22c5ce1e3bdddf589ffd147b1b7866930
gogbajbobo <grigoriev.maxim@gmail.com>
2020-03-09 14:41:12 +0300

    format

--- generator.py
+++ generator.py
@@ -48,13 +48,13 @@ def create_phantom_and_process(shape, porsty, blobns, noise_prob, num_of_angles,
     dm.save(phantom, processed_phantom, porsty, blobns, noise_prob, num_of_angles, tag)
     if preview:
         if len(phantom.shape) == 3:
-            _, axes = plt.subplots(1,2)
-            axes[0].imshow(processed_phantom[:,:,0], cmap = 'gray')
-            axes[1].imshow(phantom[:,:,0], cmap = 'gray')
+            _, axes = plt.subplots(1, 2)
+            axes[0].imshow(processed_phantom[:, :, 0], cmap='gray')
+            axes[1].imshow(phantom[:, :, 0], cmap='gray')
         else:
-            _, axes = plt.subplots(1,2)
-            axes[0].imshow(processed_phantom, cmap = 'gray')
-            axes[1].imshow(phantom, cmap = 'gray')
+            _, axes = plt.subplots(1, 2)
+            axes[0].imshow(processed_phantom, cmap='gray')
+            axes[1].imshow(phantom, cmap='gray')
 
     return np.absolute(phantom), np.absolute(processed_phantom)
 
