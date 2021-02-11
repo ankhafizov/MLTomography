33bcad2118a089b7d6c07860aaa38e1433f4000f
Anvar <ankhafizov@yandex.ru>
2021-02-11 13:52:19 +0300

    var renamed: blobiness --> characteristical_pore_length

--- phantom_generator.py
+++ phantom_generator.py
@@ -3,12 +3,12 @@ from scipy.ndimage import gaussian_filter
 from porespy.tools import norm_to_uniform
 
 
-def gen_phantom(shape, porosity, blobiness):
+def gen_phantom(shape, porosity, characteristical_pore_length):
     shape = np.array(shape)
     if np.size(shape) == 1:
         shape = np.full((3, ), int(shape))
     im = np.random.random(shape)
-    im = gaussian_filter(im, sigma=blobiness)
+    im = gaussian_filter(im, sigma=characteristical_pore_length)
     im = norm_to_uniform(im, scale=[0, 1])
     if porosity:
         im = im > porosity
