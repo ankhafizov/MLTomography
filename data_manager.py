1b4416beebf4ef92e0f45cd640b429be56630c58
Anvar <ankhafizov@yandex.ru>
2021-02-12 16:31:27 +0300

    default data_train filename changed in data_manager

--- data_manager.py
+++ data_manager.py
@@ -3,14 +3,14 @@ import pandas as pd
 
 SCRIPT_PATH = os.path.dirname(os.path.abspath(__file__))
 DATA_FOLDER_NAME = 'data_frames'
-EVRAZ_DATA = "evraz_data.csv"
+TRAIN_DATA_FILENAME = "train_dataframe.csv"
 DEFAULT_SEP = ";"
 DEFAULT_DECIMAL = ","
 
 
-def load_data(csv_file_name=EVRAZ_DATA):
+def load_data(csv_file_name=TRAIN_DATA_FILENAME):
     """ загружает csv файл
-    csv_file_name : str defaut "evraz_data.csv"
+    csv_file_name : str defaut "train_dataframe.csv"
         Название csv файла
     
     Returns
@@ -36,8 +36,8 @@ def save_data(df, file_name):
 
     file_path = os.path.join(SCRIPT_PATH, DATA_FOLDER_NAME, file_name)
 
-    if file_name == EVRAZ_DATA:
-        raise ValueError(f"Name file_name={EVRAZ_DATA} constricted")
+    if file_name == TRAIN_DATA_FILENAME:
+        raise ValueError(f"Name file_name={TRAIN_DATA_FILENAME} constricted")
     if os.path.exists(file_path):
         os.remove(file_path)
 
