980b9bcaf267802b6b965deee42ea9918d7716aa
ankhafizov <ankhafizov1998@yandex.ru>
2021-04-02 19:26:56 +0300

    added grimax solution to test

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import wavelet_cpl_extractor as wce
+import grimax_cpl_extractor as gce
 import pandas as pd
 import phantom_generator as pg
 import data_manager as dm
@@ -13,21 +14,26 @@ if __name__ == "__main__":
                                  'phantom_sigma',
                                  'calculated_sigma',
                                  'deviation_percent',
-                                 'processing_time'])
+                                 'processing_time_seconds'])
 
 
-    phantom_shape = [500, 500]
+    phantom_shape = [1, 1_000_000]
 
-    for phantom_porosity in [0.2, 0.4]:
-        for phantom_sigma in [10, 15]:
+    for phantom_porosity in [0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5]:
+        for phantom_sigma in [10, 12.5, 15, 17.5 20, 22.5, 25]:
+            phantom = ~pg.gen_phantom(phantom_shape, phantom_porosity, phantom_sigma)
             
-            for method_name in ["anvar"]:
+            for method_name in ["anvar", "grimax"]:
                 #### main body ####
                 start = timeit.default_timer()
                 
                 #TODO: add grimax
-                extractor_func = wce.extract_sigma if method_name == "anvar" else wce.extract_sigma
-                phantom = ~pg.gen_phantom(phantom_shape, phantom_porosity, phantom_sigma)
+                extractor_func = wce.extract_cpl if method_name == "anvar" else gce.sigma_estimate_2
+
+                if method_name == "grimax":
+                    phantom = phantom.T
+                print(phantom)
+
                 calculated_sigma = extractor_func(phantom)
                 
                 stop = timeit.default_timer()
