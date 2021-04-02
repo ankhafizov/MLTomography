b2bad66ef32a3f549403521f3b82a8e29f9ac07e
ankhafizov <ankhafizov1998@yandex.ru>
2021-04-02 18:44:27 +0300

    cpl --> sigma

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ if __name__ == "__main__":
 
     df = pd.DataFrame(columns = ['method_name',
                                  'phantom_porosity',
-                                 'phantom_cpl',
-                                 'calculated_cpl',
+                                 'phantom_sigma',
+                                 'calculated_sigma',
                                  'deviation_percent',
                                  'processing_time'])
 
@@ -19,25 +19,25 @@ if __name__ == "__main__":
     phantom_shape = [500, 500]
 
     for phantom_porosity in [0.2, 0.4]:
-        for phantom_cpl in [10, 15]:
+        for phantom_sigma in [10, 15]:
             
             for method_name in ["anvar"]:
                 #### main body ####
                 start = timeit.default_timer()
                 
                 #TODO: add grimax
-                extractor_func = wce.extract_cpl if method_name == "anvar" else wce.extract_cpl
-                phantom = ~pg.gen_phantom(phantom_shape, phantom_porosity, phantom_cpl)
-                calculated_cpl = extractor_func(phantom)
+                extractor_func = wce.extract_sigma if method_name == "anvar" else wce.extract_sigma
+                phantom = ~pg.gen_phantom(phantom_shape, phantom_porosity, phantom_sigma)
+                calculated_sigma = extractor_func(phantom)
                 
                 stop = timeit.default_timer()
                 #### main body ####
 
                 df = df.append({'method_name': method_name,
                                 'phantom_porosity': phantom_porosity,
-                                'phantom_cpl': phantom_cpl,
-                                'calculated_cpl': calculated_cpl,
-                                'deviation_percent': np.abs(calculated_cpl - phantom_cpl) / phantom_cpl * 100,
+                                'phantom_sigma': phantom_sigma,
+                                'calculated_sigma': calculated_sigma,
+                                'deviation_percent': np.abs(calculated_sigma - phantom_sigma) / phantom_sigma * 100,
                                 'processing_time_seconds': stop-start}, ignore_index=True)
                 
-                dm.save_dataframe(df, "cpl_extraction_contest.csv")
+                dm.save_dataframe(df, "sigma_extraction_contest.csv")
