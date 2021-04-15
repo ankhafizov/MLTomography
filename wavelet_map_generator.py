07afbac3ee47ce3a1108a47fb695ce6688410dc9
ankhafizov <ankhafizov1998@yandex.ru>
2021-04-15 15:17:05 +0300

    wavelet map functions refactored

--- wavelet_map_generator.py
+++ wavelet_map_generator.py
@@ -1,68 +1,50 @@
 
-import phantom_generator as pg
-import data_manager as dm
-import pandas as pd
-
-import matplotlib.pyplot as plt
 import numpy as np
-from scipy import signal, stats
-
+import scipy
+import pandas as pd
 from progress.bar import Bar
 
-WAVELET = signal.ricker
+import MLTomography.phantom_generator as pg
+import MLTomography.data_manager as dm
+from MLTomography.helper import invert_signal
 
 
-def invert_signal(sig):
-    sig = sig.astype(int) if sig.dtype==bool else sig
-    return sig.max() - sig
+WAVELET_TYPE = scipy.signal.ricker #mexican hat
 
 
-def get_width_of_a_row(sig):
-    widths = np.arange(1, 200)
-    cwtmatr = signal.cwt(sig, WAVELET, widths)
-    pearsons = [stats.pearsonr(row, sig)[0] for row in cwtmatr]
-    plt.plot(pearsons)
+def get_wavelet_width_of_row_signal(signal, max_width=200):
+    # TODO: add optimizer to find pearson maximum
+    get_pearson_corrcoef = lambda x1, x2: scipy.stats.pearsonr(x1, x2)[0]
+    
+    possible_widths = np.arange(1, max_width)
+    wavelet_rows = scipy.signal.cwt(signal, WAVELET_TYPE, possible_widths)
+    pearsons = [get_pearson_corrcoef(row, signal) for row in wavelet_rows]
 
     return np.argmax(pearsons)
 
 
-def get_width_phantom(phantom):
+def get_wavelet_width_for_2d_image(bin_img_2d, axis=0):
     """
-    прохожу по всем рядам и возвращаю медиану
+    axis =0, 1 or "all"
     """
+    bin_img_2d = np.asarray(bin_img_2d)
+    wavelet_widths = []
 
-    widths = []
+    if axis == 0:
+        pass
+    elif axis == 1:
+        bin_img_2d = bin_img_2d.T
+    elif axis == "all":
+        bin_img_2d = np.vstack((bin_img_2d, bin_img_2d.T))
+    else:
+        raise ValueError(f"axis must be 0, 1 or \"all\", but {axis} was given")
 
-    # все ряды по горизонтали
-    for row in phantom:
-        w = get_width_of_a_row(invert_signal(row))
-        widths.append(w)
+    for row in bin_img_2d:
+        row = invert_signal(row) # не уверен, что это необходимо. Делаю, чтобы раскладывать именно поры по вейвлетам
+        w = get_wavelet_width_of_row_signal(row)
+        wavelet_widths.append(w)
 
-    # все ряды по вертикали если есть анизатропия (закоментил для ускорения рассчетов)
-    # for row in phantom.T:
-    #     w = get_width_of_a_row(invert_signal(row))
-    #     widths.append(w)
-
-    return np.median(widths)
-
-
-def get_wavelet_widths_for_fixed_porosity(porosity,
-                                          characteristical_pore_lengths,
-                                          phantom_shape,
-                                          attempts=5):
-
-    mean_phantom_width, std_phantom_width = [], []
-
-    for cpl in characteristical_pore_lengths:
-        phantom_widths = [] 
-        for _ in range(attempts):
-            phantom = pg.gen_phantom(phantom_shape, porosity, cpl)
-            phantom_widths.append(get_width_phantom(phantom))
-    
-        mean_phantom_width.append(np.mean(phantom_widths))
-        std_phantom_width.append(np.std(phantom_widths))
-
-    return mean_phantom_width, std_phantom_width
+    return np.median(wavelet_widths)
 
 
 if __name__ == '__main__':
