7c43bc00873f76a8baa49e09f953820397b9c9ac
ankhafizov <ankhafizov1998@yandex.ru>
2021-04-16 17:49:00 +0300

    noise added to phantom generator

--- phantom_generator.py
+++ phantom_generator.py
@@ -1,9 +1,34 @@
 import numpy as np
 from scipy.ndimage import gaussian_filter
 from porespy.tools import norm_to_uniform
+import random
 
 
-def generate_phantom(shape, porosity, sigma):
+def sp_noise(image, prob):
+    '''
+    Add salt and pepper noise to image
+    prob: Probability of the noise
+    '''
+    output = np.zeros(image.shape,np.uint8)
+    thres = 1 - prob 
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            rdn = random.random()
+            if rdn < prob:
+                output[i][j] = 0
+            elif rdn > thres:
+                output[i][j] = 1
+            else:
+                output[i][j] = image[i][j]
+    return output
+
+
+def generate_phantom(shape, porosity, sigma, noise=0):
+    """
+    noise=0 - бинарный исходный фантом
+    
+    noise>0 - есть зашумление (рекомендую noise = 0.1)
+    """
     shape = np.array(shape)
     if np.size(shape) == 1:
         shape = np.full((3, ), int(shape))
@@ -12,4 +37,6 @@ def generate_phantom(shape, porosity, sigma):
     img = norm_to_uniform(img, scale=[0, 1])
     if porosity:
         img = img > porosity
+    
+    img = sp_noise(img, noise).astype(bool)
     return img
